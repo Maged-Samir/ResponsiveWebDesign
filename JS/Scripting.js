17904ef2e92c05dfcb890effdc30515be58b542b
Maged-Samir <magedsamerkamel@gmail.com>
2023-12-29 17:42:42 +0200

    Update Paths

---
+++ JS/Scripting.js
@@ -0,0 +1,29 @@
+// Show or hide the scroll-to-top button based on the scroll position
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scroll-to-top").style.display = "block";
+    } else {
+      document.getElementById("scroll-to-top").style.display = "none";
+    }
+  };
+  
+  // Scroll to the top of the page when the button is clicked
+  function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+
+  //  // Start the carousel and set the interval to 1 second (1000 milliseconds)
+  //  document.addEventListener('DOMContentLoaded', function() {
+  //   var carousel = new bootstrap.Carousel(document.getElementById('carousel1'), {
+  //     interval: 5000
+  //   });
+  // });
+
+     // Start the carousel and set the interval to 1 second (1000 milliseconds)
+     document.addEventListener('DOMContentLoaded', function() {
+      var carousel = new bootstrap.Carousel(document.getElementById('carousel1'), {
+        interval: 5000
+      });
+    });
